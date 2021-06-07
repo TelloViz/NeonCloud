f2ff44c2ba571e873dc850d45942248a30870a9a
Tello- <lollisjosh@csu.fullerton.edu>
2021-06-07 15:17:55 -0700

    Renamed backdrop scripts, formatted and flattened in hichy

--- Assets/Scripts/BackdropInitializer.cs
+++ Assets/Scripts/BackdropInitializer.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class InitBackdrop : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class BackdropInitializer : MonoBehaviour{
+    void Start(){
         var objectToColor = GetComponent<ColorManager>();
         objectToColor.ChangeToRandomColorInRange();
     }
