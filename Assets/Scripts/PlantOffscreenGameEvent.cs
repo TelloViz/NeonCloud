7bbe84dafbe95fed681356033c170c9fa6f6e3fa
Tello- <lollisjosh@csu.fullerton.edu>
2021-06-10 18:57:57 -0700

    Can't keep me down

---
+++ Assets/Scripts/PlantOffscreenGameEvent.cs
@@ -0,0 +1,7 @@
+using SOEvents.Events;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Game Event (PlantOffscreenGameEvent)", menuName = "Events/New Game Event (PlantOffscreenGameEvent)")]
+public class PlantOffscreenGameEvent : OffscreenGameEvent
+{
+}
