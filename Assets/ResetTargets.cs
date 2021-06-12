852e661949b2f1e07041d9c113de53f2a1ac569f
Tello- <lollisjosh@csu.fullerton.edu>
2021-06-11 23:05:50 -0700

    Got parallax changed to a simpler check for position systmem

--- Assets/ResetTargets.cs
+++ Assets/ResetTargets.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 
 
-public class BGResetTarget : MonoBehaviour
+public class ResetTargets : MonoBehaviour
 {
     [SerializeField] private Transform resetFromTarget;
     [SerializeField] private Transform resetToTarget;
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if(this.gameObject.transform.position.x <= resetFromTarget.position.x){
+        if (this.gameObject.transform.position.x <= resetFromTarget.position.x)
+        {
             this.gameObject.transform.SetPositionAndRotation(new Vector3(resetToTarget.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z), this.gameObject.transform.rotation);
         }
     }
