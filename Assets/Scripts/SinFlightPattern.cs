3ee1804512eabce286611093178eef2622ae282f
Tello- <lollisjosh@csu.fullerton.edu>
2021-06-14 19:08:00 -0700

    Trying not to lose these updates

--- Assets/Scripts/SinFlightPattern.cs
+++ Assets/Scripts/SinFlightPattern.cs
@@ -11,11 +11,13 @@ namespace FlightPattern {
         [SerializeField] private float magnitude;
         [SerializeField] private float period;
 
-        // TODO: Figure out a different way to get the sinData to this object, currently it's being passed in and this makes no sense actually
         public override void IncrementRigidbody(Rigidbody rb, Vector3 startPos, float dt) {
-            Vector3 mov = new Vector3(rb.position.x + xVelocity * dt,startPos.y + magnitude * Mathf.Sin(rb.position.x / period), rb.position.z);
-            rb.position = mov;
 
+            Vector3 newPos;
+            newPos.x = rb.position.x + xVelocity * dt;
+            newPos.y = startPos.y + magnitude * Mathf.Sin(rb.position.x / period);
+            newPos.z = rb.position.z;
+            rb.position = newPos;
         }
 
     }
