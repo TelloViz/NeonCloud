43667909f4cd43c10036f47563bec5ab7a9a6a31
Tello- <tellovisionstudio@gmail.com>
2021-06-25 21:29:20 -0700

    Added long comment to plant monster script

--- Assets/Scripts/PlantMonster.cs
+++ Assets/Scripts/PlantMonster.cs
@@ -7,6 +7,16 @@ public class PlantMonster : MonoBehaviour {
 
     private Rigidbody rb;
     [SerializeField] private Vector3 startingPos;
+    
+    /* I made fp (BaseFlightPattern a SerializeField in order to allow a 
+     * designer to swap these patterns out easily in the editor. I chose
+     this approach vs. using GetComponent<BaseFlightPattern> because I thought
+    maybe a Plant monster (in this case) would potentially be holding multiple FlightPattern
+    scripts. The problem then becomes, how does the code end of things (here in the plant script) 
+    know how many are available to choose from. It allows a designer to drag and drop from these assigned 
+    to the plant monster but the logic would have to poll for them by name, array index or something? Doesn't seem wise.*/
+
+    // TODO I think I need to make a Flight Handler that calls these various available patterns for you... ask Vast
     [SerializeField] BaseFlightPattern fp;
 
 
