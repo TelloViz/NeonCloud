58a5290e61ece5395ee431975b4efe11784b5d50
Tello- <tellovisionstudio@gmail.com>
2021-06-30 18:49:47 -0700

    Script folder organization and some state namespace added

---
+++ Assets/Scripts/GameState/InMainMenu.cs
@@ -0,0 +1,26 @@
+﻿using Vast.StateMachine;
+using UnityEngine;
+
+namespace GameState
+{
+    public class InMainMenu : State
+    {
+        private const string DEFAULT_NAME = "MenuState";
+
+        public InMainMenu(string name = DEFAULT_NAME)
+        {
+            Name = name;
+            Debug.Log(name + " state created...");
+        }
+
+        public override void OnEnter() { }
+
+        public override void OnExit() { }
+
+        public override void Update() { }
+
+        public override void FixedUpdate() { }
+
+    }
+
+}
