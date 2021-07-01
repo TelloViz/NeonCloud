f9452a64ae9286a610774adf8dd797af4f48b06c
Tello- <tellovisionstudio@gmail.com>
2021-06-30 20:27:55 -0700

    Added FixedUpdateActiveState to IStateMachine

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vast.StateMachine;
+
+public class GameStateManager : MonoBehaviour
+{
+
+    [SerializeField] private StateMachine stateMachine;
+    private State inMenuState;
+    private State playingState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inMenuState = stateMachine.AddState(new GameState.InMainMenu());
+        playingState = stateMachine.AddState(new GameState.Playing());
+
+
+        if (stateMachine.ContainsState(inMenuState))
+        {
+            stateMachine.ChangeState(inMenuState);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateMachine.UpdateActiveState();
+    }
+
+    private void FixedUpdate()
+    {
+        stateMachine.FixedUpdateActiveState();
+    }
+}
