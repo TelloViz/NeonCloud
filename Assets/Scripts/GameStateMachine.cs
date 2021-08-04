b2e352e34a340148ed63e6dc7b5778e3ac1a7ee6
Tello- <tellovisionstudio@gmail.com>
2021-08-04 14:09:59 -0700

    General Commit

--- Assets/Scripts/GameStateMachine.cs
+++ Assets/Scripts/GameStateMachine.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Vast.StateMachine;
+using GameState;
 
 public class GameStateMachine : MonoBehaviour
 {
+    // Serialized StateMachine allows for changing states in editor at run-time
     [SerializeField] private StateMachine stateMachine;
-    private State inMenuState;
+
+    private InMainMenu inMainMenuState;
+    private Playing inPlayingState;
 
     // Note to self: Awake called before Start(), even if script it disabled.
     private void Awake()
     {
-        
+
     }
 
     private void OnDestroy()
@@ -27,9 +31,11 @@ public class GameStateMachine : MonoBehaviour
     private void HandleStateChange(State newState)
     /// <summary>Hook into our underlying StateMachine field, This will get called on each state change</summary>
     {
+
         Debug.Log(this.name + " says, the state is now " + newState.Name);
 
         // TODO: Handle state changes, deligate to individual state handling objects maybe
         // TODO: Determine flow of control for this
 
     }
+}
